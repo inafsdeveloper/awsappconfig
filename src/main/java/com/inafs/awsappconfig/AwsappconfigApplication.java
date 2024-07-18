9fbc7f6320fdc2d22756716822060514ead03f30
inafsdeveloper <inafsdeveloper@gmail.com>
2024-07-18 12:32:51 -0700

    Initial Commit

---
+++ src/main/java/com/inafs/awsappconfig/AwsappconfigApplication.java
@@ -0,0 +1,13 @@
+package com.inafs.awsappconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsappconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsappconfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inafs/awsappconfig/AwsappconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inafs.awsappconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsappconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
